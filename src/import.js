ea0d406e829795995359b2ab9905d8920a3c0013
Jocelyne28 <jocy_alch@hotmail.com>
2022-03-31 13:17:11 -0600

    Se agrega el proyecto

---
+++ src/import.js
@@ -0,0 +1,53 @@
+import src1 from './imagenes/Sabadillo/sabadillo1.jpg';
+import src2 from './imagenes/Sabadillo/sabadillo2.jpg';
+import src3 from './imagenes/Sabadillo/sabadillo3.jpg';
+import src4 from './imagenes/Sabadillo/sabadillo4.jpg';
+import src5 from './imagenes/Sabadillo/sabadillo5.jpg';
+import src6 from './imagenes/Sabadillo/sabadillo6.jpg';
+
+const imagenes = [
+    {
+        src: src1,
+        momento: 'Epic 1'
+    },
+    {
+        src: src1,
+        momento: 'Epic 1'
+    },
+    {
+        src: src2,
+        momento: 'Epic 2'
+    },
+    {
+        src: src2,
+        momento: 'Epic 2'
+    },
+    {
+        src: src3,
+        momento: 'Epic 3'
+    },
+    {
+        src: src3,
+        momento: 'Epic 3'
+    },    {
+        src: src4,
+        momento: 'Epic 4'
+    },    {
+        src: src4,
+        momento: 'Epic 4'
+    },    {
+        src: src5,
+        momento: 'Epic 5'
+    },    {
+        src: src5,
+        momento: 'Epic 5'
+    },    {
+        src: src6,
+        momento: 'Epic 6'
+    },    {
+        src: src6,
+        momento: 'Epic 6'
+    }
+];
+
+export {imagenes}
